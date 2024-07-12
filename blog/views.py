8507e04496d3b24f9043ddd06d647acc255ddcd3
DerrickLegacy <ahaabwederrick67@gmail.com>
2024-07-12 21:18:35 +0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author':'Ahaabwe Derrick',
+        'title': 'How to pass without reading',
+        'content':'First Post Content',
+        'date_posted':'August 28, 2024'
+    },
+    {
+        'author':'Joe Doe',
+        'title': 'Blog Post Two',
+        'content':'Second Post Content',
+        'date_posted':'August 29, 2024'
+    }
+]
+
+# Create your views here.
+# Point/map blog views to templates to render
+# Pass Data to views
+def home(request):
+    context = {
+        'posts':posts
+    }    
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'About'})
