fb179673295d624c2321b1a42e541035e5c965ae
DerrickLegacy <ahaabwederrick67@gmail.com>
2024-07-12 23:13:04 +0300

    database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author':'Ahaabwe Derrick',
-        'title': 'How to pass without reading',
-        'content':'First Post Content',
-        'date_posted':'August 28, 2024'
-    },
-    {
-        'author':'Joe Doe',
-        'title': 'Blog Post Two',
-        'content':'Second Post Content',
-        'date_posted':'August 29, 2024'
-    }
-]
+# posts = [
+#     {
+#         'author':'Ahaabwe Derrick',
+#         'title': 'How to pass without reading',
+#         'content':'First Post Content',
+#         'date_posted':'August 28, 2024'
+#     },
+#     {
+#         'author':'Joe Doe',
+#         'title': 'Blog Post Two',
+#         'content':'Second Post Content',
+#         'date_posted':'August 29, 2024'
+#     }
+# ]
 
 # Create your views here.
 # Point/map blog views to templates to render
 # Pass Data to views
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all(),
     }    
     return render(request, 'blog/home.html', context)
 
